75b717ca65427270a6f96aaf4a49e5eabf937003
alons126 <alonsportes@mail.tau.ac.il>
2025-05-28 14:36:31 +0300

    C12_sim_G18_2070MeV 100 files

--- HipoLooper/HipoLooper.cpp
+++ HipoLooper/HipoLooper.cpp
@@ -391,11 +391,16 @@ void HipoLooper() {
         if (HistoList_electron_cuts[i]->InheritsFrom("TH1D")) {
             HistoList_electron_cuts[i]->Draw();
 
+            TLine *target_location;
+
             if (target_status == "C12") {
-                TLine *target_location = new TLine((2.5 - 3.0), 0., (2.5 - 3.0), gPad->GetFrame()->GetY2());
+                target_location = new TLine((2.5 - 3.0), 0., (2.5 - 3.0), gPad->GetFrame()->GetY2());
             } else if (target_status == "Ar40") {
-                TLine *target_location = new TLine((-2.5 - 3.0), 0., (-2.5 - 3.0), gPad->GetFrame()->GetY2());
+                target_location = new TLine((-2.5 - 3.0), 0., (-2.5 - 3.0), gPad->GetFrame()->GetY2());
             }
+
+            target_location->SetLineColor(kBlue);
+            target_location->Draw("same");
         } else if (HistoList_electron_cuts[i]->InheritsFrom("TH2D")) {
             HistoList_electron_cuts[i]->Draw("colz");
 
