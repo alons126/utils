f276a2c169b7dbe80c5c6710634571e349285f5c
alons126 <alonsportes@mail.tau.ac.il>
2025-05-28 14:49:08 +0300

    C12_sim_G18_2070MeV 100 files

--- HipoLooper/HipoLooper.cpp
+++ HipoLooper/HipoLooper.cpp
@@ -405,7 +405,7 @@ void HipoLooper() {
             target_location->SetLineColor(kBlue);
             target_location->Draw("same");
 
-            auto Legend = new TLegend(gStyle->GetStatX(), gStyle->GetStatY() - 0.2, gStyle->GetStatX() - 0.25, gStyle->GetStatY() - 0.3);
+            auto Legend = new TLegend(gStyle->GetStatX(), gStyle->GetStatY() - 0.35, gStyle->GetStatX() - 0.25, gStyle->GetStatY() - 0.4);
             TLegendEntry *target_location_entry = Legend->AddEntry(target_location, ("Spec target location = " + basic_tools::ToStringWithPrecision(spec_target_location,2) + " cm").c_str(), "l");
 
             Legend->Draw("same");
