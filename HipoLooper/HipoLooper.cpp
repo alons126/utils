5bdcde812f778eff2f25bb49f0f2af60fdefd2e8
alons126 <alonsportes@mail.tau.ac.il>
2025-06-04 14:01:20 +0300

    C sim 4GeV full

--- HipoLooper/HipoLooper.cpp
+++ HipoLooper/HipoLooper.cpp
@@ -53,8 +53,8 @@ void HipoLooper() {
 
     // Simulation samples:
 
-    std::string InputFiles = BaseDir + "/C12/G18_10a_00_000/2070MeV_Q2_0_02_Ar40_test/reconhipo/*.hipo";
-    // std::string InputFiles = BaseDir + "/C12/G18_10a_00_000/4029MeV_Q2_0_25_Ar40_test/reconhipo/*.hipo";
+    // std::string InputFiles = BaseDir + "/C12/G18_10a_00_000/2070MeV_Q2_0_02_Ar40_test/reconhipo/*.hipo";
+    std::string InputFiles = BaseDir + "/C12/G18_10a_00_000/4029MeV_Q2_0_25_Ar40_test/reconhipo/*.hipo";
 
     // std::string InputFiles = BaseDir + "/Ar40/G18_10a_00_000/2070MeV_Q2_0_02_Ar40_test/reconhipo/*.hipo";
     // std::string InputFiles = BaseDir + "/Ar40/G18_10a_00_000/4029MeV_Q2_0_25_Ar40_test/reconhipo/*.hipo";
