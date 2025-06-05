e3bab002f54c536389572fd1a5540cabb40bcab2
alons126 <alonsportes@mail.tau.ac.il>
2025-06-05 12:17:11 +0300

    new clas12ana test 13

--- HipoLooper/framework/classes/clas12ana/clas12ana.cpp
+++ HipoLooper/framework/classes/clas12ana/clas12ana.cpp
@@ -77,7 +77,8 @@ void clas12ana::Run(const std::unique_ptr<clas12::clas12reader> &c12) {
               (!EcalEdgeCuts(el) && f_ecalEdgeCuts) ||       // ECAL edge cuts
               (!checkVertex(el) && f_vertexCuts) ||          // Vertex cut
               (!DCEdgeCuts(el) && f_DCEdgeCuts) ||           // DC edge cut
-              (el->par()->getP() < 0.8)))                    // minium 800 MeV/c cut for electrons in class
+              (el->par()->getP() < 0.8 && false)))           // minium 800 MeV/c cut for electrons in class
+            //   (el->par()->getP() < 0.8)))                    // minium 800 MeV/c cut for electrons in class
             setByPid(el);
     });
 
