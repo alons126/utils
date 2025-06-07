7d3a7a8c797897a859bc5ca59f58c4fd04ed0795
alons126 <alonsportes@mail.tau.ac.il>
2025-06-07 13:15:24 +0300

    updated plots test 5

--- HipoLooper/HipoLooper.cpp
+++ HipoLooper/HipoLooper.cpp
@@ -2701,13 +2701,22 @@ void HipoLooper() {
                     *first_flags[particle_key] = false;
                     ++plot_counter;
                 } else {
+
+
                     std::cout << "\n\nTitle: " << title << "\n";
+
 
                     for (int sector = 1; sector <= 6; ++sector) {
                         std::string sector_str = "sector " + std::to_string(sector);
+
+
+                        std::cout << "sector: " << sector << "\n";
+                        std::cout << "*sector_flags[particle_key][sector]: " << *sector_flags[particle_key][sector] << "\n";
+                        std::cout << "basic_tools::FindSubstring(title, sector_str): " << basic_tools::FindSubstring(title, sector_str) << "\n\n";
+
+
                         if (*sector_flags[particle_key][sector] && basic_tools::FindSubstring(title, sector_str)) {
                             std::cout << "Title pass: " << title << "\n\n";
-
 
                             std::string bookmark_title = label + " plots - " + sector_str;
                             std::string sanitized_bookmark_title = histogram_functions::SanitizeForBookmark(bookmark_title);
