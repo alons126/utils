a4db7257404c9b7fea6c216ff5b390e5a5fd74fd
alons126 <alonsportes@mail.tau.ac.il>
2025-06-11 09:18:31 +0300

    Make gitc.sh executable

---
+++ HipoLooper/framework/scripts/git/gitc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# source "$(dirname "$0")/../environment/system_colors.sh"
+# source ../environment/system_colors.sh
+
+# Define your constant prefix
+HipoLooperCodeVersion=14
+GIT_COMMIT_PREFIX="[${HipoLooperCodeVersion}_HipoLooper_v${HipoLooperCodeVersion}] "
+# GIT_COMMIT_PREFIX="[MyProject] "
+# echo -e "${COLOR_START}Committing with prefix: ${GIT_COMMIT_PREFIX}${COLOR_END}"
+
+# Commit with the prefix and user input
+git add .
+git commit -m "${GIT_COMMIT_PREFIX}$*"
