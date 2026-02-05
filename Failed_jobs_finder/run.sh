dc7fe69c2ae2b2b99e7092c6e6964774fdedf625
alons126 <alons126@gmail.com>
2026-02-05 09:59:36 +0200

    Rerun for Ar40/GEM21_11a_00_000/2070MeV_Q2_0_02

--- Failed_jobs_finder/run.sh
+++ Failed_jobs_finder/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PyDir=/pnfs/genie/scratch/users/asportes/2N_Analysis_Samples/Ar40/G18_10a_00_000/402962MeV
+export PyDir=/pnfs/genie/scratch/users/asportes/2N_Analysis_Samples/Ar40/GEM21_11a_00_000/2070MeV_Q2_0_02
 export List_File="list.txt"
 ls  PyDir/"master-routine_validation_01-eScattering >" + PyDir/List_File
 
